e50da133cb819fa364a3420fc3cc401e5af37ab3
Rafael Pessoni <rafael.pessoni@outlook.com>
2016-09-10 15:38:03 -0300

    alterações no conteudo de estudo do primeiro capitulo

--- Chapter 01/conteudo/exemplo-query-completa.sql
+++ Chapter 01/conteudo/exemplo-query-completa.sql
@@ -1,6 +1,17 @@
-SELECT country, YEAR(hiredate) AS yearhired, COUNT(*) AS numemployees
-FROM HR.Employees
-WHERE hiredate >= '20030101'
-GROUP BY country, YEAR(hiredate)
-HAVING COUNT(*) > 1
-ORDER BY country , yearhired DESC;
+
+
+
+
+
+
+
+SELECT	country, 
+		YEAR(hiredate) AS yearhired, 
+		COUNT(*) AS numemployees
+	FROM HR.Employees
+	WHERE hiredate >= '20030101'
+	GROUP BY country, YEAR(hiredate)
+	HAVING COUNT(*) > 1
+	ORDER BY country , yearhired DESC;
+
+SELECT * FROM hR.Employees order by region desc
