cba0e4f396f7c538ce2be39cd09caf8d9fdc6f75
Vinicius Mussak <vinicius.mussak@outlook.com>
2016-10-01 13:11:12 -0300

    Conteudo

--- Chapter 04/estudo/conteudo.sql
+++ Chapter 04/estudo/conteudo.sql
@@ -28,4 +28,109 @@ SELECT	S.companyname AS supplier,
 			ON S.supplierid = P.supplierid
 	WHERE S.country = N'Japan';
 
+-- Usar WHERE pra Juntar e JOIN pra Filtrar
+SELECT	S.companyname AS supplier,
+		S.country,
+		P.productid,
+		P.productname,
+		P.unitprice
+	FROM Production.Suppliers AS S
+		INNER JOIN Production.Products AS P
+			ON S.supplierid = P.supplierid
+			AND S.country = N'Japan';
+
+
+-- Exemplo do alias
+-- Join com a mesma tabela (auto-relacionamento)
+SELECT	E.empid,
+		E.firstname + N' ' + E.lastname AS employee,
+		M.firstname + N' ' + M.lastname AS manager
+	FROM HR.Employees AS E
+		INNER JOIN HR.Employees AS M
+			ON E.mgrid = M.empid;
+
+
+-- LEFT OUTER JOIN
+-- Aqui, usar WHERE ou ON faz diferença
+SELECT	S.companyname AS supplier,
+		S.country,
+		P.productid, 
+		P.productname, 
+		P.unitprice
+	FROM Production.Suppliers AS S
+		LEFT OUTER JOIN Production.Products AS P
+			ON S.supplierid = P.supplierid
+	WHERE S.country = N'Japan';
+
+
+-- Falar sobre RIGHT JOIN e FULL OUTER JOIN
+
+
+--Combinando vários JOINS
+SELECT	S.companyname AS supplier,
+		S.country,
+		P.productid,
+		P.productname,
+		P.unitprice,
+		C.categoryname
+	FROM Production.Suppliers AS S
+		LEFT OUTER JOIN Production.Products AS P
+			ON S.supplierid = P.supplierid
+		INNER JOIN Production.Categories AS C
+			ON C.categoryid = P.categoryid
+	WHERE S.country = N'Japan';
+
+
+
+-- Resolvendo o problema acima:
+SELECT	S.companyname AS supplier,
+		S.country,
+		P.productid,
+		P.productname,
+		P.unitprice,
+		C.categoryname
+	FROM Production.Suppliers AS S
+		LEFT OUTER JOIN
+			(Production.Products AS P
+				INNER JOIN Production.Categories AS C
+					ON C.categoryid = P.categoryid
+			)
+			ON S.supplierid = P.supplierid
+	WHERE S.country = N'Japan';
+
+
+-- Subqueries
+
+SELECT	productid,
+		productname,
+		unitprice
+	FROM Production.Products
+	WHERE unitprice = (SELECT MIN(unitprice) FROM Production.Products);
+
+
+SELECT	productid,
+		productname,
+		unitprice
+	FROM Production.Products
+	WHERE supplierid IN (SELECT supplierid FROM Production.Suppliers WHERE country = N'Japan');
+
+
+-- Subqueries correlacionadas
+
+SELECT	categoryid,
+		productid,
+		productname,
+		unitprice
+	FROM Production.Products AS P1
+	WHERE unitprice = (
+		SELECT MIN(unitprice) 
+			FROM Production.Products AS P2 
+			WHERE P2.categoryid = P1.categoryid
+	);
+
+
+--Table expressions (CTEs)
+
+
+
 
